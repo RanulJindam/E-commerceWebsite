e2709a115549ac4c63d7cb4d16c6990431ab53ca
RanulJindam <143937912+RanulJindam@users.noreply.github.com>
2023-09-03 16:42:16 +0530

    Add files via upload
    https://e-commerce-website-by-rahul.netlify.app/

---
+++ E_commerce_project/orderplaced.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#clothsSelect').click(function(){
+        window.location.href="./index.html";
+    });
+    $('#gadgetSelect').click(function(){
+        window.history.back();
+    });
+
+});
